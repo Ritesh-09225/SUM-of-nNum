77c0f747943307a2c691b6be685daee6d5ccc3a8
Ritesh Vishwakarma <riteshvish94@gmail.com>
2025-08-24 14:00:38 +0530

    Add files via upload

---
+++ Program2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+int n;
+printf("Enter the value of n: ");
+scanf("%d",&n);
+int sum=(n*(n+1))/2;
+printf("The sum of first n natural numbers: ");
+printf("%d",sum);
+}
